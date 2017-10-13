fb5a5622b5f7341a754ef23cfb66f368b66599c6
AlanHuang <ishuangte@163.com>
2017-10-13 10:56:48 +0800

    add python3 folder

---
+++ iPython/awesome-python3-webapp/www/test/tcp_server.py
@@ -0,0 +1,31 @@
+# 导入socket库:
+import socket
+import threading,time
+
+#创建socket
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#监听端口
+s.bind(('127.0.0.1',9999))
+#最大连接数5
+s.listen(5)
+print('Waiting for connection...')
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
+while True:
+	#接受一个新连接
+	sock,addr=s.accept()
+	#创建线程来处理TCP连接
+	t = threading.Thread(target=tcplink,args=(sock,addr))
+	t.start()
+
